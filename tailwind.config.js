e04c444f4e83db8050746c52379a734b34dcd769
Binary Bond <binarybond03@gmail.com>
2024-12-26 14:41:54 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Route/*.{html,js}",
-     "./node_modules/flowbite/**/*.js"
+  content: [
+    "./html/*.html", // Scans all HTML files in the html folder
+    "./js/*.js", // Scans all JS files in the js folder
+    "./css/*.css", // Ensures all relevant CSS files are included
   ],
   theme: {
     extend: {},
